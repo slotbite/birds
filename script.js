3e1b7928b019921c8a6a5b9392aef118c2d45c98
Cristián Andrés <zoneslotbite@gmail.com>
2022-12-13 23:32:18 -0300

    Static title

--- script.js
+++ script.js
@@ -4610,6 +4610,6 @@ const toggleButton = document.getElementById("toggle-dark-mode");
 toggleButton.addEventListener("click", () => {
   document.body.classList.toggle("dark-mode");
   toggleButton.textContent = document.body.classList.contains("dark-mode")
-    ? "🌙"
-    : "🌞";
+    ? "🌞"
+    : "🌙";
 });
